58d7a6d60a95abb560bf3aa2653d05cf86bd8002
Kelly Redding <kelly@kellyredding.com>
2013-05-01 13:59:35 -0500

    updates to work with the latest Dassets (v0.4.0)

--- test/support/config/assets.rb
+++ test/support/config/assets.rb
@@ -5,5 +5,7 @@ Dassets.configure do |c|
   c.root_path File.expand_path("../..", __FILE__)
   c.engine 'scss', Dassets::Sass::Engine, :syntax => 'scss'
   c.engine 'sass', Dassets::Sass::Engine, :syntax => 'sass'
+  c.cache = nil
+  c.file_store 'public'
 
 end
--- test/system/digest_tests.rb
+++ test/system/digest_tests.rb
@@ -10,19 +10,19 @@ module Dassets::Sass
     setup do
       @source_file = File.join(Dassets.config.source_path, "from_scss.scss")
       File.open(@source_file, 'w'){ |f| f.write(@factory.scss) }
-      @output_file = File.join(Dassets.config.output_path, "from_scss.css")
-      @exp_output_content = @factory.scss_compiled
+      @compiled_file = File.join(Dassets.config.root_path, "public/from_scss-ffcdabded94a78022e9c92e06e311f69.css")
+      @exp_compiled_content = @factory.scss_compiled
 
-      FileUtils.rm_f @output_file
+      FileUtils.rm_f @compiled_file
     end
 
     should "produce a css file with the source scss compiled to css" do
       assert_file_exists @source_file
-      assert_not_file_exists @output_file
+      assert_not_file_exists @compiled_file
       Dassets.digest_source_files
 
-      assert_file_exists @output_file
-      assert_equal @exp_output_content, File.read(@output_file)
+      assert_file_exists @compiled_file
+      assert_equal @exp_compiled_content, File.read(@compiled_file)
     end
 
   end
@@ -32,19 +32,19 @@ module Dassets::Sass
     setup do
       @source_file = File.join(Dassets.config.source_path, "from_sass.sass")
       File.open(@source_file, 'w'){ |f| f.write(@factory.sass) }
-      @output_file = File.join(Dassets.config.output_path, "from_sass.css")
-      @exp_output_content = @factory.sass_compiled
+      @compiled_file = File.join(Dassets.config.root_path, "public/from_sass-7173fff1fe11e06f1339beceaf4a9857.css")
+      @exp_compiled_content = @factory.sass_compiled
 
-      FileUtils.rm_f @output_file
+      FileUtils.rm_f @compiled_file
     end
 
     should "produce a css file with the source sass compiled to css" do
       assert_file_exists @source_file
-      assert_not_file_exists @output_file
+      assert_not_file_exists @compiled_file
       Dassets.digest_source_files
 
-      assert_file_exists @output_file
-      assert_equal @exp_output_content, File.read(@output_file)
+      assert_file_exists @compiled_file
+      assert_equal @exp_compiled_content, File.read(@compiled_file)
     end
 
   end
