e36d6064643611730e30a660e9fd360dd9791a2d
Kelly Redding <kelly@kellyredding.com>
2013-06-17 10:33:38 -0500

    some further test removals due to previous system test removals
    Just more cleanups.

--- test/helper.rb
+++ test/helper.rb
@@ -7,13 +7,7 @@ $LOAD_PATH.unshift(File.expand_path("../..", __FILE__))
 # require pry for debugging (`binding.pry`)
 require 'pry'
 
-require 'pathname'
-TEST_SUPPORT_ROOT = Pathname.new(File.expand_path('../support', __FILE__))
-
 ENV['DASSETS_TEST_MODE']   = 'yes'
-ENV['DASSETS_ASSETS_FILE'] = 'test/support/config/assets'
-require 'dassets'
-Dassets.init
 
 require 'test/support/factory'
 class Assert::Context
--- test/support/config/assets.rb
+++
@@ -1,7 +0,0 @@
-require 'dassets'
-require 'dassets-sass'
-
-Dassets.config.source TEST_SUPPORT_ROOT.join("app/assets") do |s|
-  s.engine 'scss', Dassets::Sass::Engine, :syntax => 'scss'
-  s.engine 'sass', Dassets::Sass::Engine, :syntax => 'sass'
-end
